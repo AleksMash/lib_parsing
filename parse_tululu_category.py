5feaac31af6d1620a5a61732d5cc15dcfc4138fb
AleksMash <amashukov@yandex.ru>
2023-03-03 19:38:55 +0600

    Format code

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -1,15 +1,15 @@
-import sys
-import os
 import argparse
 import json
-from urllib.parse import urljoin
+import os
+import sys
 from pathlib import Path
+from urllib.parse import urljoin
 
 import requests
 from bs4 import BeautifulSoup
-from pathvalidate import sanitize_filepath, sanitize_filename
+from pathvalidate import sanitize_filename, sanitize_filepath
 
-from tululu import (parse_book_page, get_response, download_book, download_image)
+from tululu import download_book, download_image, get_response, parse_book_page
 
 
 def get_books_urls(genre_url, page_start, page_end=None):
@@ -21,30 +21,46 @@ def get_books_urls(genre_url, page_start, page_end=None):
         except (requests.HTTPError, requests.ConnectionError):
             continue
         else:
-            soup = BeautifulSoup(response.text, 'lxml')
-            selector = 'div#content table.d_book'
+            soup = BeautifulSoup(response.text, "lxml")
+            selector = "div#content table.d_book"
             tables = soup.select(selector)
             for table in tables:
-                href = table.tr.a['href']
-                book_urls.append({
-                    'url': urljoin(genre_url, href),
-                    'id': href.removeprefix('/b').removesuffix('/')
-                })
+                href = table.tr.a["href"]
+                book_urls.append(
+                    {
+                        "url": urljoin(genre_url, href),
+                        "id": href.removeprefix("/b").removesuffix("/"),
+                    }
+                )
     return book_urls
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Load txt books from tululu.org from pages")
+    parser = argparse.ArgumentParser(
+        description="Load txt books from tululu.org from pages"
+    )
     parser.add_argument("--first_page", type=int, help="Первая страница", default=1)
     parser.add_argument("--last_page", type=int, help="Последняя страница")
-    parser.add_argument("-df", "--dest_folder", type=str, default='books', help="папка, в которую будут скачаны книги"
-                                                                                ", изображения обложек и помещен "
-                                                                                "json-файл с информацией о книгах.")
-    parser.add_argument('--skip_imgs', action='store_true', help='Не скачивать изображения обложек')
-    parser.add_argument('--skip_txt', action='store_true', help='Не скачивать книги')
-    parser.add_argument('--json_path',type=str , help='указать свой путь к *.json файлу с информацией'
-                                            ' о книгах')
-
+    parser.add_argument(
+        "-df",
+        "--dest_folder",
+        type=str,
+        default="books",
+        help=(
+            "папка, в которую будут скачаны книги"
+            ", изображения обложек и помещен "
+            "json-файл с информацией о книгах."
+        ),
+    )
+    parser.add_argument(
+        "--skip_imgs", action="store_true", help="Не скачивать изображения обложек"
+    )
+    parser.add_argument("--skip_txt", action="store_true", help="Не скачивать книги")
+    parser.add_argument(
+        "--json_path",
+        type=str,
+        help="указать свой путь к *.json файлу с информацией о книгах",
+    )
     args = parser.parse_args()
     first_page = args.first_page
     last_page = args.last_page
@@ -52,97 +68,124 @@ def main():
     skip_imgs = args.skip_imgs
     skip_txt = args.skip_txt
     json_path = args.json_path
-    genre_first_page_url = 'https://tululu.org/l55/'
+    genre_first_page_url = "https://tululu.org/l55/"
     if first_page <= 0:
-        print('Номера страниц должны быть >= 1')
+        print("Номера страниц должны быть >= 1")
         return
     response = get_response(genre_first_page_url)
-    soup = BeautifulSoup(response.text, 'lxml')
-    max_page = int(soup.select('a.npage')[-1].text)
+    soup = BeautifulSoup(response.text, "lxml")
+    max_page = int(soup.select("a.npage")[-1].text)
     if last_page:
         if last_page <= 0:
-            print('Номера страниц должны быть >= 1')
+            print("Номера страниц должны быть >= 1")
             return
         if last_page < first_page:
-            print('Последня страница не может быть меньше первой')
+            print("Последня страница не может быть меньше первой")
             return
         if first_page > max_page:
-            print(f'Первая страница, указанная при вызове скрипта больше'
-                  f' максимального числа страниц ({max_page})')
+            print(
+                "Первая страница, указанная при вызове скрипта больше"
+                f" максимального числа страниц ({max_page})"
+            )
             return
         if last_page > max_page:
-            print(f'Последняя страница, указанная при вызове скрипта, превышает '
-                  f'максимальное число страниц ({max_page})\n'
-                  f'будут скачаны книги со страниц {first_page}-{max_page}')
+            print(
+                "Последняя страница, указанная при вызове скрипта, превышает "
+                f"максимальное число страниц ({max_page})\n"
+                f"будут скачаны книги со страниц {first_page}-{max_page}"
+            )
             last_page = max_page + 1
         else:
             last_page = last_page + 1
     else:
-        print(f'Будут скачаны книги со страниц {first_page}-{max_page}')
+        print(f"Будут скачаны книги со страниц {first_page}-{max_page}")
         last_page = max_page + 1
     book_urls = get_books_urls(genre_first_page_url, first_page, last_page)
     folder = sanitize_filepath(folder)
-    Path(folder, 'images').mkdir(parents=True, exist_ok=True)
+    Path(folder, "images").mkdir(parents=True, exist_ok=True)
     if json_path:
         json_path = sanitize_filepath(json_path)
         json_folder = os.path.split(json_path)[0]
         Path(json_folder).mkdir(parents=True, exist_ok=True)
     else:
-        json_path = Path(folder, 'book_info.json')
+        json_path = Path(folder, "book_info.json")
     about_books = []
     count = 0
-    print('Ссылки на страницы книг получены.',
-          '\nТекст книг скачан не будет (задана опция --skip_txt)' if skip_txt else '',
-          '\nИзображения обложек скачаны не будут (задана опция --skip_imgs)' if skip_imgs else '',
-          '\nНачинаем обработку ссылок...\n')
+    print(
+        "Ссылки на страницы книг получены.",
+        "\nТекст книг скачан не будет (задана опция --skip_txt)" if skip_txt else "",
+        "\nИзображения обложек скачаны не будут (задана опция --skip_imgs)"
+        if skip_imgs
+        else "",
+        "\nНачинаем обработку ссылок...\n",
+    )
     for num, book_url in enumerate(book_urls):
         print(f'Обработка {book_url["url"]}')
-        url = book_url['url']
-        book_id = book_url['id']
+        url = book_url["url"]
+        book_id = book_url["id"]
         try:
-            response = get_response(book_url['url'])
+            response = get_response(book_url["url"])
         except requests.HTTPError:
-            print(f'Не удается найти главную страницу книги по адресу: {url}'
-                  , file=sys.stderr)
+            print(
+                f"Не удается найти главную страницу книги по адресу: {url}",
+                file=sys.stderr,
+            )
         except requests.ConnectionError as e:
-            print('Проблема с интернет-соединением', file=sys.stderr)
+            print("Проблема с интернет-соединением", file=sys.stderr)
             print(e)
             sys.exit()
         else:
             parsed_book = parse_book_page(response.text, response.url)
             if not skip_txt:
                 try:
-                    params = {'id': book_id}
+                    params = {"id": book_id}
                     count += 1
-                    filepath = os.path.join(folder, sanitize_filename(f'{count}-я книга. {parsed_book["title"]}.txt'))
-                    response = download_book('https://tululu.org/txt.php', params, filepath)
-                    parsed_book['book_path'] = filepath
+                    filepath = os.path.join(
+                        folder,
+                        sanitize_filename(
+                            f'{count}-я книга. {parsed_book["title"]}.txt'
+                        ),
+                    )
+                    response = download_book(
+                        "https://tululu.org/txt.php", params, filepath
+                    )
+                    parsed_book["book_path"] = filepath
                 except requests.HTTPError:
-                    print(f'Не удается найти ссылку для загрузки книги: {response.url}', file=sys.stderr)
-                    parsed_book['book_path'] = 'Не удалось найти ссылку для загрузки книги'
+                    print(
+                        f"Не удается найти ссылку для загрузки книги: {response.url}",
+                        file=sys.stderr,
+                    )
+                    parsed_book[
+                        "book_path"
+                    ] = "Не удалось найти ссылку для загрузки книги"
                     count -= 1
                 except requests.ConnectionError as e:
-                    print('Проблема с интернет-соединением', file=sys.stderr)
+                    print("Проблема с интернет-соединением", file=sys.stderr)
                     print(e)
                     sys.exit()
             if not skip_imgs:
                 try:
-                    img_path = Path(Path.cwd(), folder,
-                                    'images', parsed_book['img_file_name'])
-                    download_image(parsed_book['img_url'], img_path)
+                    img_path = Path(
+                        Path.cwd(), folder, "images", parsed_book["img_file_name"]
+                    )
+                    download_image(parsed_book["img_url"], img_path)
                 except requests.HTTPError:
-                    print(f'Не удается скачать изображение с URL: {parsed_book["img_url"]}', file=sys.stderr)
+                    print(
+                        "Не удается скачать изображение с URL:"
+                        f' {parsed_book["img_url"]}',
+                        file=sys.stderr,
+                    )
                 except requests.ConnectionError as e:
-                    print('Проблема с интернет-соединением', file=sys.stderr)
+                    print("Проблема с интернет-соединением", file=sys.stderr)
                     print(e)
                     sys.exit()
             about_books.append(parsed_book)
     if not skip_txt:
-        print(f'\nВсего скачано книг: {count}')
+        print(f"\nВсего скачано книг: {count}")
     # books_json = json.dump(about_books, ensure_ascii=False, indent=4)
     with open(json_path, "w") as file:
         json.dump(about_books, file, ensure_ascii=False, indent=4)
 
 
 if __name__ == "__main__":
-   main()
+    main()
--- tululu.py
+++ tululu.py
@@ -1,12 +1,12 @@
+import argparse
 import os
 import sys
-import argparse
 from pathlib import Path
-from urllib.parse import urljoin, unquote
+from urllib.parse import unquote, urljoin
 
-from bs4 import BeautifulSoup
-from pathvalidate import sanitize_filepath, sanitize_filename
 import requests
+from bs4 import BeautifulSoup
+from pathvalidate import sanitize_filename, sanitize_filepath
 from retry import retry
 
 
@@ -16,43 +16,43 @@ def parse_book_page(html, page_url):
     Arguments:
         html - html (str) content of the web page.
         page_url - URL of the page"""
-    soup = BeautifulSoup(html, 'lxml')
-    splitted_text = soup.select('h1')[0].text.split('::')
+    soup = BeautifulSoup(html, "lxml")
+    splitted_text = soup.select("h1")[0].text.split("::")
     book_title, author = map(lambda s: s.strip(), splitted_text)
-    img_path = soup.select_one('div.bookimage img')['src']
+    img_path = soup.select_one("div.bookimage img")["src"]
     img_url = urljoin(page_url, img_path)
     img_file_name = os.path.basename(unquote(img_path))
-    comments_tags = soup.select('div.texts')
+    comments_tags = soup.select("div.texts")
     comments = [c.span.text for c in comments_tags]
-    genre_tags = soup.select('span.d_book a')
+    genre_tags = soup.select("span.d_book a")
     genres = [a.text for a in genre_tags]
     return {
-        'title': book_title,
-        'author': author,
-        'img_url': img_url,
-        'comments': comments,
-        'genres': genres,
-        'img_file_name': img_file_name,
+        "title": book_title,
+        "author": author,
+        "img_url": img_url,
+        "comments": comments,
+        "genres": genres,
+        "img_file_name": img_file_name,
     }
 
 
 @retry(requests.ConnectionError, jitter=0.5, tries=5)
 def download_image(url, filename):
     """!!! filename must be a valid path
-     to the image file. Ensure that it is properly prepared!!!"""
+    to the image file. Ensure that it is properly prepared!!!"""
     response = requests.get(url)
     response.raise_for_status()
     if not os.path.exists(filename):
-        with open(filename, 'wb') as file:
+        with open(filename, "wb") as file:
             file.write(response.content)
 
 
 @retry(requests.ConnectionError, jitter=0.5, tries=5)
 def download_book(url, params, filename):
     """!!! filename must be a valid path
-     to the *.txt file !!! Ensure that it is properly prepared"""
+    to the *.txt file !!! Ensure that it is properly prepared"""
     response = get_response(url, params)
-    with open(filename, 'wt', encoding='UTF-8') as file:
+    with open(filename, "wt", encoding="UTF-8") as file:
         file.write(response.text)
     return response
 
@@ -70,55 +70,76 @@ def main():
     parser = argparse.ArgumentParser(description="Load txt books from tululu.org")
     parser.add_argument("first_id", type=int, help="ID первой книги")
     parser.add_argument("last_id", type=int, help="ID последней книги")
-    parser.add_argument("-f", "--folder", type=str, default='books', help="папка, в которую будут скачаны книги")
+    parser.add_argument(
+        "-f",
+        "--folder",
+        type=str,
+        default="books",
+        help="папка, в которую будут скачаны книги",
+    )
     args = parser.parse_args()
     first_id = args.first_id
     last_id = args.last_id
     folder = args.folder
-    if last_id<first_id:
-        print('ID2 должен быть больше или равен ID1')
+    if last_id < first_id:
+        print("ID2 должен быть больше или равен ID1")
         return
-    Path('images').mkdir(parents=True, exist_ok=True)
+    Path("images").mkdir(parents=True, exist_ok=True)
     san_folder = sanitize_filepath(folder)
     Path(san_folder).mkdir(parents=True, exist_ok=True)
-    comments_delimiter = ', \n'
-    for book_id in range(first_id,last_id+1):
+    comments_delimiter = ", \n"
+    for book_id in range(first_id, last_id + 1):
         try:
-            response = get_response(f'https://tululu.org/b{book_id}/')
+            response = get_response(f"https://tululu.org/b{book_id}/")
         except requests.HTTPError as e:
-            print(f'Не удается найти главную страницу книги по адресу:'
-                  f'https://tululu.org/b{book_id}/', file=sys.stderr)
+            print(
+                "Не удается найти главную страницу книги по адресу:"
+                f"https://tululu.org/b{book_id}/",
+                file=sys.stderr,
+            )
         except requests.ConnectionError as e:
-            print('Проблема с интернет-соединением', file=sys.stderr)
+            print("Проблема с интернет-соединением", file=sys.stderr)
             print(e)
             sys.exit()
         else:
             book_parsed = parse_book_page(response.text, response.url)
-            filepath = os.path.join(san_folder, sanitize_filename(f'{book_id}. {book_parsed["title"]}.txt'))
+            filepath = os.path.join(
+                san_folder, sanitize_filename(f'{book_id}. {book_parsed["title"]}.txt')
+            )
             try:
-                params = {'id': book_id}
-                response = download_book('https://tululu.org/txt.php', params, filepath)
+                params = {"id": book_id}
+                response = download_book("https://tululu.org/txt.php", params, filepath)
             except requests.HTTPError as e:
-                print(f'Не удается скачать книгу с URL: {response.url}', file=sys.stderr)
+                print(
+                    f"Не удается скачать книгу с URL: {response.url}", file=sys.stderr
+                )
             except requests.ConnectionError as e:
-                print('Проблема с интернет-соединением', file=sys.stderr)
+                print("Проблема с интернет-соединением", file=sys.stderr)
                 print(e)
                 sys.exit()
             try:
-                download_image(book_parsed['img_url'], os.path.join('images', book_parsed['img_file_name']))
+                download_image(
+                    book_parsed["img_url"],
+                    os.path.join("images", book_parsed["img_file_name"]),
+                )
             except requests.HTTPError as e:
-                print(f'Не удается скачать изображение с URL: {book_parsed["img_url"]}', file=sys.stderr)
+                print(
+                    f'Не удается скачать изображение с URL: {book_parsed["img_url"]}',
+                    file=sys.stderr,
+                )
             except requests.ConnectionError as e:
-                print('Проблема с интернет-соединением', file=sys.stderr)
+                print("Проблема с интернет-соединением", file=sys.stderr)
                 print(e)
                 sys.exit()
-            info_file_path = os.path.join(san_folder, f'{book_id}. info.txt')
-            with open(info_file_path, 'wt') as file:
+            info_file_path = os.path.join(san_folder, f"{book_id}. info.txt")
+            with open(info_file_path, "wt") as file:
                 file.write(f'Наименование: {book_parsed["title"]}\n')
                 file.write(f'Автор: {book_parsed["author"]}\n')
-                file.write(f'Путь к файлу книги: {os.path.abspath(filepath)}\n')
+                file.write(f"Путь к файлу книги: {os.path.abspath(filepath)}\n")
                 file.write(f'Жанры: {", ".join(book_parsed["genres"])}\n\n')
-                file.write(f'Комментарии:\n\n{comments_delimiter.join(book_parsed["comments"])}')
+                file.write(
+                    f'Комментарии:\n\n{comments_delimiter.join(book_parsed["comments"])}'
+                )
 
 
 if __name__ == "__main__":
